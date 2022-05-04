c0b57d82e9916c90d95c6cbcae6da9af723b0cdf
OrigamiL <1714573172@qq.com>
2022-05-04 14:23:51 +0800

    5.4 afternoon commit

---
+++ servlet10/src/com/weifang/javaweb/servlet/AServlet.java
@@ -0,0 +1,23 @@
+package com.weifang.javaweb.servlet;
+
+import com.weifang.javaweb.javabean.User;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class AServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        User user = new User(1,"jack");
+        request.setAttribute("user",user);
+        //request.getRequestDispatcher("/b").forward(request,response);
+        response.sendRedirect("/servlet10/b?user="+user.getName()+"");
+
+    }
+}
